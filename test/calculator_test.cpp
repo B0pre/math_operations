467851cc4b7ef233b28132dfb0c8a1480e9d13e3
bopre <bopre>
2021-07-21 22:25:23 +0300

    Refactoring in tests

--- test/calculator_test.cpp
+++ test/calculator_test.cpp
@@ -11,9 +11,3 @@ TEST(MathOperTest, SampleTests)
     ASSERT_EQ(0.5, calculate("1/2"));
     ASSERT_EQ(2, calculate("-1 * -2"));
 }
-
-int main(int argc, char ** argv)
-{
-    testing::InitGoogleTest(&argc, argv);
-    return RUN_ALL_TESTS();
-}
